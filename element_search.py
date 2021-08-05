0d5bd614b8984bc9088f7146decddb6e601f7a29
Sorin <ciobanusorin1986@gmail.com>
2021-08-05 21:18:28 +0100

    added test_for_xpath() function

--- element_search.py
+++ element_search.py
@@ -23,6 +23,41 @@ def log_event(event, logfile='log.txt'):
         return False
 
 
+def test_for_xpath(driver, element, element_name='', check_once=True, continuous=False, timeout=30, logfile='log.txt'):
+    elem = None
+    element_name = element if element_name == '' else element_name
+    t = 0
+    err = -1
+    log_event(f"Testing for '{element_name}' xpath")
+    while (check_once or continuous) and t < timeout:
+        try:
+            t += 1
+            print(f"searching for '{element}' xpath...")
+            time.sleep(1)
+            elem = driver.find_element_by_xpath(element)
+            # failing to find results in error, so except: executes
+            print('Found it')
+            log_event(f"Found '{element_name}' xpath", logfile)
+            break
+        except Exception as e:
+            err = e
+            if continuous:
+                print(f'looking for {timeout - t} more times:\n{e}')
+            # not to repeat if only once was desired
+            if check_once:
+                #print(f"'{element_name}' not present")
+                log_event(f"'{element_name}' not present", logfile)
+                check_once = False
+
+    if t == timeout and elem is None:
+        print(f"Time Out !!!\nCouldn't find {element_name}")
+        log_event(f"Timeout, fail to find '{element_name}': {err if err else 'Unknown Reason'}", logfile)
+        # probably there will always be a reason , so that might disappear
+        # but will leave it for now
+
+    return elem
+
+
 def main():
     # here we write simple code to test the functions
     log_event('if there is no log.txt, create it, if it exists, add this line in it')
