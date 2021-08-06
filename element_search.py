fd207b7e8c9bc425f35b52effbec168ddd3b409f
Sorin <ciobanusorin1986@gmail.com>
2021-08-06 16:40:20 +0100

    Add search_by_id() function

--- element_search.py
+++ element_search.py
@@ -97,5 +97,17 @@ def search_by_xpath(driver, element, element_name='', check_once=True, continuou
                           logfile=logfile)
 
 
+def search_by_id(driver, element, element_name='', check_once=True, continuous=False, timeout=30, logfile='log.txt'):
+
+    return search_element('id',
+                          driver,
+                          element,
+                          element_name=element_name,
+                          check_once=check_once,
+                          continuous=continuous,
+                          timeout=timeout,
+                          logfile=logfile)
+
+
 if __name__ == '__main__':
     print('Refer to   usage_example.py   for usage examples ...')
